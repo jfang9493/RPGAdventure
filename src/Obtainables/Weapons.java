fd8066c3a929a9756c4e12978a1e6b6fe10a6be7
BT_1N3_18 <@>
2019-02-06 09:47:38 -0500

    Balance Patch 1.1

--- src/Obtainables/Weapons.java
+++ src/Obtainables/Weapons.java
@@ -2,9 +2,9 @@ package Obtainables;
 
 public class Weapons extends Items {
     String[] giveWeapons = {
-            "Raggity Stick",//Start Weapon     Set Dmg:100
-            "Knights Blade",//Drop Rate:75%    Set Dmg:150
-            "BFF Sword",//Drop Rate:30%        Set Dmg:200
+            "Raggity Stick",//Start Weapon     Set Dmg:70
+            "Knights Blade",//Drop Rate:75%    Set Dmg:75
+            "BFF Sword",//Drop Rate:30%        Set Dmg:100
             "B.O.R.K"};//Drop Rate:1%          Set Dmg:300
     public int giveAttack(String name)
     {
@@ -12,17 +12,17 @@ public class Weapons extends Items {
         System.out.println(prob1);
         if(prob1==1 )
         {
-            return 300;
+            return 150;
         }
         prob1 = (int)(Math.random()*10)+1;
         if(prob1==1 || prob1==2 || prob1==3)
         {
-            return 200;
+            return 100;
         }
         prob1 = (int)(Math.random()*4)+1;
         if(prob1==1 || prob1==2 || prob1==3)
         {
-            return 150;
+            return 75;
         }
         return 0;
     }
