c7c1147e4e1524e2f22c5484dfdd87c8da39bb3f
BT_1N3_20 <@>
2019-02-06 09:39:39 -0500

    @@@11

--- src/Maps/Map.java
+++ src/Maps/Map.java
@@ -1,4 +1,39 @@
 package Maps;
 
-public class Map {
+public class Map implements mappicker
+{
+    private mappicker map;
+    private int x;
+    private int y;
+    public Map(mappicker map, int x, int y)
+    {
+        this.map = map;
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public String determineMap() {
+        return null;
+    }
+
+    @Override
+    public int getWidth() {
+        return 0;
+    }
+
+    @Override
+    public int getLength() {
+        return 0;
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public int getBasicPixel(int x, int y) {
+        return 0;
+    }
 }
