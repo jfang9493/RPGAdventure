12f63f1c4a47d6991ab167ae6d557f66bf5c726b
BT_1N3_18 <@>
2019-02-07 09:08:27 -0500

    Vincent Touching mystical class

--- src/Obtainables/Defense.java
+++ src/Obtainables/Defense.java
@@ -2,20 +2,34 @@ package Obtainables;
 
 public class Defense extends Items {
     String[] giveDefense = {
-            "Raggity Woodplank",
-            "Adequate Armor",
-            "Knights Vow",
-            "Sunfire Cape",
-            "Ultimate Mystical Shiny Armor"};
+            "Raggity Woodplank",//Starting Defense     Defense:0
+            "Adequate Armor",//Drop Rate:80%           Defense:25
+            "Knights Vow",//Drop Rate:50%              Defense 40
+            "Something Armor",//Drop Rate:20%          Defense: 60
+            "Ultimate Mystical Shiny Armor"};//Drop Rate:1% Defense 125
 
-    public int giveAttack() {
-        return 0;
-    }
+
     public int giveDefense() {
-        return 0;
-    }
-
-    public int givemp() {
+        int prob1 = (int)(Math.random()*100)+1;
+        if(prob1==1)
+        {
+            return 125;
+        }
+        prob1 = (int)(Math.random()*10)+1;
+        if(prob1==1||prob1==2)
+        {
+            return 60;
+        }
+        prob1 = (int)(Math.random()*2)+1;
+        if(prob1==1)
+        {
+            return 40;
+        }
+        prob1= (int)(Math.random()*10)+1;
+        if(prob1!=1 && prob1!=2)
+        {
+            return 25;
+        }
         return 0;
     }
 }
