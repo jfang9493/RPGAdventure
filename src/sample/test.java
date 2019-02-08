aa85c6c475934bce0033364136cd1f2eacd4c290
BT_1N3_19 <@>
2019-02-08 09:29:08 -0500

    2.8 #2

--- src/sample/test.java
+++ src/sample/test.java
@@ -1,11 +1,14 @@
 package sample;
 
+import Characters.Player;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+
+import java.util.ArrayList;
 
 public class test extends Application {
     /**
@@ -17,12 +20,14 @@ public class test extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        primaryStage.setTitle("Load Image");
+        primaryStage.setTitle("The RPG Game");
 
         StackPane sp = new StackPane();
         Image img = new Image("MapFiles/starting room.png");
         ImageView imgView = new ImageView(img);
         sp.getChildren().add(imgView);
+
+        Player player = new Player(100,100,50,50,1,400,400,new ArrayList<>());
 
         //Adding HBox to the scene
         Scene scene = new Scene(sp);
