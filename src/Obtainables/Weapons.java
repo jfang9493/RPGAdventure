ee7a17b4c0f3143d23865868d4bc0c8714ee275e
User <vzhao2047@bths.edu>
2019-02-11 21:19:25 -0500

    Defense Health Item Weapons Obtainable Package //Mysticals will be part of the player

--- src/Obtainables/Weapons.java
+++ src/Obtainables/Weapons.java
@@ -8,41 +8,33 @@ public class Weapons extends Items {
             "B.O.R.K"};//Drop Rate:1%          Set Dmg:300
     private int effectAtk;
     private String weaponName;
-    public Weapons(String type,String name, int effect){
-        super(type);
-        effectAtk=effect;
-        weaponName=name;
-    }
-    public void getAttack()
-    {
-        int prob1= (int)(Math.random()*100)+1;
-        if(prob1==1 )
-        {
-            effectAtk= 150;
-            weaponName="B.O.R.K";
-        }
-        prob1 = (int)(Math.random()*10)+1;
-        if(prob1==1 || prob1==2 || prob1==3)
-        {
-            effectAtk= 100;
-            weaponName="BFF Sword";
-        }
-        prob1 = (int)(Math.random()*4)+1;
-        if(prob1==1 || prob1==2 || prob1==3)
-        {
-            effectAtk= 75;
-            weaponName="Knights Blade";
-        }
-        effectAtk= 0;
-        weaponName="Mpthomg";
-    }
-    @Override
-    public String toString() {
-        return (super.toString() +
-                "\nItem Name: " + weaponName+
-                "\nItem Effect: " + effectAtk);
 
+    public Weapons(String type, String name, int effect) {
+        super(type, name, effect);
+        effectAtk = effect;
+        weaponName = name;
     }
+
+    public void getAttack() {
+        int prob1 = (int) (Math.random() * 100) + 1;
+        if (prob1 == 1) {
+            effectAtk = 150;
+            weaponName = "B.O.R.K";
+        }
+        prob1 = (int) (Math.random() * 10) + 1;
+        if (prob1 == 1 || prob1 == 2 || prob1 == 3) {
+            effectAtk = 100;
+            weaponName = "BFF Sword";
+        }
+        prob1 = (int) (Math.random() * 4) + 1;
+        if (prob1 == 1 || prob1 == 2 || prob1 == 3) {
+            effectAtk = 75;
+            weaponName = "Knights Blade";
+        }
+        effectAtk = 0;
+        weaponName = "Mpthomg";
+    }
+}
     /*
     public int giveAttack()
     {
@@ -64,4 +56,4 @@ public class Weapons extends Items {
         return 0;
     }
     */
-}
+
